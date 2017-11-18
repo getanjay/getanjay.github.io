0a27de63f228079cc500d5f1c58cf639199bf48a
Muhammad Arsie Aziz <arsieaziz@gmail.com>
2017-11-18 11:45:06 +0700

    update mobile

--- js/default.js
+++ js/default.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){});
+$(document).ready(function(){$("#icon-mobile").click(function(){$(".sidebar").toggle(),$(".menu").toggle()}),$("a").click(function(){$(".sidebar").hide()})});
